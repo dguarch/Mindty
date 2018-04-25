9a8ffc0f73d72cf6254b88b74701c11f2356dde0
ivansyn <37666993+ivan7878@users.noreply.github.com>
2018-04-25 15:52:19 +0200

    !

--- js/localStorage.js
+++ js/localStorage.js
@@ -2,4 +2,4 @@ document.getElementById('btn_form').onclick = (function(){
     localStorage.setItem('professor', [document.getElementById('code_modulo').value, document.getElementById('nombre_modulo').value]);
 });
 
-// ==================================================API LOCAL STORAGE PROFESSOR.HTML==========================================================//
+// ==================================================!!!!!!!!!API LOCAL STORAGE PROFESSOR!!!!!!!==========================================================//
