879061c321c8281bd9b05ec4f848c66a062034f5
A4Alumno <a4alumno@aules.local>
2018-04-25 16:49:51 +0200

    Desarrollo de la validación de coordinador

---
+++ js/formValidation2.js
@@ -0,0 +1,25 @@
+var elem = null;
+
+const elementosEspera = () => {
+    elem = document.querySelector("#codigo_curso");
+    elem.onblur = validarCampo;
+};
+
+const validarCampo = () => {
+    let _valid = true;
+    let _formValues = {};
+    $('form .error').remove();
+    $('#codigo_curso').each(function () {
+        let validezNombre = this.validity;
+        console.log(validezNombre);
+        if (validezNombre.patternMismatch) {
+            $('#codigo_curso').after('<div class="error"><i class="far fa-times-circle"></i> Código inválido.</div>');
+        }
+        if (validezNombre.valueMissing) {
+            $('#codigo_curso').after('<div class="error"><i class="far fa-times-circle"></i> Código inválido.</div>');
+        }
+        _formValues[this.name] = this.value;
+    });
+}
+
+elementosEspera();
