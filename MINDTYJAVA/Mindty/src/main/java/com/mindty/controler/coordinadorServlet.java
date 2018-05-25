64c45fd1d756c1d24ecd06b68cba36a6e0c33825
hector <hector.torres.vicente@gmail.com>
2018-05-25 19:23:36 +0200

    actualizado el pro en JAVA--buscar en la carpeta MINTYJAVA

--- MINDTYJAVA/Mindty/src/main/java/com/mindty/controler/coordinadorServlet.java
+++ MINDTYJAVA/Mindty/src/main/java/com/mindty/controler/coordinadorServlet.java
@@ -1,14 +1,15 @@
 package com.mindty.controler;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class coordinadorServlet
- */
+
+@WebServlet("/coordinador")
 public class coordinadorServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -24,8 +25,7 @@ public class coordinadorServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("coordinador.jsp").forward(request, response);
 	}
 
 	/**
--- MINDTYJAVA/Mindty/src/main/java/com/mindty/controler/profesorServlet.java
+++ MINDTYJAVA/Mindty/src/main/java/com/mindty/controler/profesorServlet.java
@@ -2,13 +2,13 @@ package com.mindty.controler;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class profesorServlet
- */
+@WebServlet("/profesor")
+
 public class profesorServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -24,8 +24,7 @@ public class profesorServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("profesor.jsp").forward(request, response);
 	}
 
 	/**
