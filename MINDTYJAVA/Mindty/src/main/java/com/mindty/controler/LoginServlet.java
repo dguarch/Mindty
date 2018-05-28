23abfc79b9b5949aabf9be073e235d1ffd7a795a
youssef <youssef.ait.ferreria@gamil.com>
2018-05-28 16:41:51 +0200

    Login Funcional

--- MINDTYJAVA/Mindty/src/main/java/com/mindty/controler/LoginServlet.java
+++ MINDTYJAVA/Mindty/src/main/java/com/mindty/controler/LoginServlet.java
@@ -12,43 +12,43 @@ import javax.servlet.http.HttpSession;
 import com.mindty.ddbb.BBDD;
 import com.mindty.modelos.Usuario;
 
-
 @WebServlet("/login")
 
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	HttpSession session = request.getSession();
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-	if (session.getAttribute("usuario")!=null) {
-		response.sendRedirect("");
-	}else {
-		request.getRequestDispatcher("login.jsp").forward(request, response);	
-	}
-	
+		HttpSession session = request.getSession();
+
+		if (session.getAttribute("usuario") != null) {
+			response.sendRedirect("");
+		} else {
+			request.getRequestDispatcher("login.jsp").forward(request, response);
 		}
-	
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		String email = request.getParameter("email");
-		String pass = request.getParameter("password");
-		Usuario unUsuarioEncontrado = BBDD.Login(email, pass);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		String user = request.getParameter("usuario");
+		String pass = request.getParameter("contraseña");
+		Usuario unUsuarioEncontrado = BBDD.Login(user, pass);
 
 		if (unUsuarioEncontrado != null) {
 			HttpSession session = request.getSession();
-			session.setAttribute("email", unUsuarioEncontrado.getUsuario());
+			session.setAttribute("usuario", unUsuarioEncontrado.getUsuario());
 			String muro = null;
 			switch (unUsuarioEncontrado.getTipo()) {
 			case "coordenador":
-				muro = "coordenador";
+				muro = "/coordenador";
 				break;
 			case "profesor":
-				muro = "profesor";
+				muro = "/profesor";
 				break;
 			case "alumno":
-				muro = "alumno";
+				muro = "/alumno";
 				break;
 			}
 			response.sendRedirect(muro);
